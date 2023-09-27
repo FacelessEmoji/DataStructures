9dec6d13699d71c5b2f7bc8d4bb112b7f35cdaaf
Emoji <studyemoji@mail.ru>
2023-09-27 13:18:32 +0300

    added isEmpty, count, expand, enqueue, dequeue methods

--- src/study/data/structures/Queue/Main.java
+++ src/study/data/structures/Queue/Main.java
@@ -2,7 +2,20 @@ package study.data.structures.Queue;
 
 public class Main {
     public static void main(String[] args) {
+        UnidirectionalQueue<Integer> queue = new UnidirectionalQueue<>(1);
 
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+
+        System.out.println("Dequeue: " + queue.dequeue()); // Dequeue: 1
+        System.out.println("Count: " + queue.count()); // Count: 2
+
+        queue.enqueue(4);
+        queue.enqueue(5);
+        queue.enqueue(6); // Expand will be called here
+
+        System.out.println("Count: " + queue.count()); // Count: 5
 
     }
 }
--- src/study/data/structures/Queue/UnidirectionalQueue.java
+++ src/study/data/structures/Queue/UnidirectionalQueue.java
@@ -1,15 +1,64 @@
 package study.data.structures.Queue;
 
+import java.util.Arrays;
+
 public class UnidirectionalQueue<T> {
-    private T[] array;
+    private Object[] array;
     private int size;
     private int head;
     private int tail;
 
     public UnidirectionalQueue(int capacity) {
-        this.array = (T[]) new Object[capacity];
+        this.array = new Object[capacity];
         this.size = 0;
         this.head = 0;
-        this.tail = 0;
+        this.tail = -1;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public int count(){
+        return size;
+    }
+
+    public void expand(){
+        System.out.println("Array before:");
+        Arrays.stream(array).forEach(System.out::println);
+
+        int newCapacity = array.length * 2;
+        Object[] newArray = new Object[newCapacity];
+        System.out.println("Loop");
+        for (int i = 0; i < size; i++) {
+            System.out.println((head + i) % array.length);
+            newArray[i] = array[(head + i) % array.length];
+        }
+        array = newArray;
+        head = 0;
+        tail = size - 1;
+
+        System.out.println("Array after:");
+        Arrays.stream(array).forEach(System.out::println);
+    }
+
+    public void enqueue(T element) {
+        if (size == array.length) {
+            expand();
+        }
+        tail = (tail + 1) % array.length;
+        array[tail] = element;
+        size++;
+    }
+
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        T element = (T) array[head];
+        array[head] = null;
+        head = (head + 1) % array.length;
+        size--;
+        return element;
     }
 }
